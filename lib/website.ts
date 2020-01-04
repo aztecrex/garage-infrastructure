8131acb71a9137942ce84a5c7c98f687157115b0
Greg Wiley <gwiley@cj.com>
2020-01-04 14:15:34 -0800

    website

--- lib/website.ts
+++ lib/website.ts
@@ -6,18 +6,18 @@ import * as s3 from '@aws-cdk/aws-s3';
 import * as iam from '@aws-cdk/aws-iam';
 
 const SITE_NAME='garage.gregwiley.com';
-const VALIDATION_ZONE='gregwiley.com';
+const TARGET_ZONE='gregwiley.com';
 
 export class Website extends cdk.Construct {
     constructor(scope: cdk.Construct) {
         super(scope, 'website');
-        // const cert = new Certificate(this);
-        const service = new StaticWeb(this);
+        const cert = new Certificate(this);
+        const service = new StaticWeb(this, cert);
     }
 }
 
 class StaticWeb extends cdk.Stack {
-    constructor(scope: cdk.Construct) {
+    constructor(scope: cdk.Construct, certificate: Certificate) {
         // this stack depends on being in a specific account, assuming
         // it is the same as the CLI will use, but probably should be made more
         // explicit so it fails with useful information. the trouble is i don't
@@ -37,40 +37,59 @@ class StaticWeb extends cdk.Stack {
             resources: [origin.arnForObjects('*')],
         }));
 
-        // const cdn = new cloudfront.CloudFrontWebDistribution(this, 'distribution', {
-        //     originConfigs: [
-        //         {
-        //           s3OriginSource: {
-        //           s3BucketSource: origin,
-        //           originAccessIdentity: identity,
-        //           },
-        //           behaviors : [ {isDefaultBehavior: true}]
-        //         }
-        //       ],
+        const cdn = new cloudfront.CloudFrontWebDistribution(this, 'distribution', {
+            originConfigs: [
+                {
+                  s3OriginSource: {
+                  s3BucketSource: origin,
+                  originAccessIdentity: accessId,
+                  },
+                  behaviors : [ {isDefaultBehavior: true}]
+                }
+              ],
+            aliasConfiguration: {
+                acmCertRef: certificate.certificateRef,
+                names: [SITE_NAME],
+            }
 
-        // });
+        });
+
+        new route53.CnameRecord(this, 'domain', {
+            domainName: cdn.domainName,
+            zone: targetZone(this),
+            recordName: SITE_NAME,
+            ttl: cdk.Duration.minutes(5),
+        });
     }
 }
 
-// class Certificate extends cdk.Stack {
-//     constructor(scope: cdk.Construct) {
-//         // this stack depends on being in a specific account, assuming
-//         // it is the same as the CLI will use, but probably should be made more
-//         // explicit so it fails with useful information. the trouble is i don't
-//         // think it's a good idea to store an account id in a source repository.
-//         super(scope, 'certificate', {
-//             env: {
-//               account: process.env.CDK_DEFAULT_ACCOUNT,
-//               region: 'us-east=1'
-//           }});
+function targetZone(scope: cdk.Construct) {
+    return route53.HostedZone.fromLookup(scope, 'validation-zone', {
+        domainName: TARGET_ZONE,
+    });
+}
 
-//         new acm.DnsValidatedCertificate(this, 'certificate', {
-//             domainName: 'garage.gregwiley.com',
-//             hostedZone: route53.HostedZone.fromLookup(this, 'validation-zone', {
-//                 domainName: 'gregwiley.com',
-//             }),
-//         });
+class Certificate extends cdk.Stack {
+    public readonly certificateRef;
+
+    constructor(scope: cdk.Construct) {
+        // this stack depends on being in a specific account, assuming
+        // it is the same as the CLI will use, but probably should be made more
+        // explicit so it fails with useful information. the trouble is i don't
+        // think it's a good idea to store an account id in a source repository.
+        super(scope, 'certificate', {
+            env: {
+              account: process.env.CDK_DEFAULT_ACCOUNT,
+              region: 'us-east-1'
+          }});
+
+        const cert = new acm.DnsValidatedCertificate(this, 'certificate', {
+            domainName: SITE_NAME,
+            hostedZone: targetZone(this),
+        });
+
+        this.certificateRef = cert.certificateArn;
 
 
-//     }
-// }
+    }
+}
