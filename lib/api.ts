c97681f67373f75b3457d88c2073d7d9211a76ba
Greg Wiley <gwiley@cj.com>
2020-01-04 20:29:19 -0800

    cors

--- lib/api.ts
+++ lib/api.ts
@@ -9,92 +9,97 @@ const TARGET_ZONE = 'gregwiley.com';
 export class API extends cdk.Construct {
     constructor(scope: cdk.Construct) {
         super(scope, 'api');
-        new Compute(this);
-        new Gateway(this);
+        const compute = new Compute(this);
+        new Gateway(this, compute);
     }
 }
 
 const DUMB_NODE_CODE = `
 exports.handler = function (event, context) {
-	context.succeed({statusCode: 200});
+	context.succeed({
+        statusCode: 200,
+        body: JSON.stringify(event)
+    });
+};
+`;
+
+const CANNED_STATUS_CODE = `
+exports.handler = function (event, context) {
+	context.succeed({
+        statusCode: 200,
+        body: JSON.stringify({position: 'Unknown', want: 'DOWN',}),
+        headers: {
+            "Content-Type" : "application/json",
+            "Access-Control-Allow-Origin" : "*",
+            "Allow" : "GET, OPTIONS",
+            "Access-Control-Allow-Methods" : "GET, OPTIONS",
+            "Access-Control-Allow-Headers" : "*"
+        }
+    });
+};
+`;
+
+const CANNED_OPERATE_CODE = `
+exports.handler = function (event, context) {
+	context.succeed({
+        statusCode: 204,
+        body: "",
+        headers: {
+            "Content-Type" : "application/json",
+            "Access-Control-Allow-Origin" : "*",
+            "Allow" : "POST, OPTIONS",
+            "Access-Control-Allow-Methods" : "POST, OPTIONS",
+            "Access-Control-Allow-Headers" : "*"
+        }
+    });
 };
 `;
 
 class NodeFunction extends lambda.Function {
-    constructor(scope: cdk.Construct, id: string) {
+    constructor(scope: cdk.Construct, id: string, code?: string) {
         super(scope, id, {
             runtime: lambda.Runtime.NODEJS_10_X,
-            code: lambda.Code.fromInline(DUMB_NODE_CODE),
+            code: lambda.Code.fromInline(code || DUMB_NODE_CODE),
             handler: 'index.handler',
         });
     }
 }
 
 class Compute extends cdk.Stack {
+    public readonly statusFunction;
+    public readonly operateFunction;
     constructor(scope: cdk.Construct) {
         super(scope, 'compute');
-        new NodeFunction(this, 'status');
-        new NodeFunction(this, 'operate');
+        this.statusFunction = new NodeFunction(this, 'status', CANNED_STATUS_CODE);
+        this.operateFunction = new NodeFunction(this, 'operate', CANNED_OPERATE_CODE);
     }
 }
 
 class Gateway extends cdk.Stack {
-    constructor(scope: cdk.Construct) {
+    constructor(scope: cdk.Construct, compute: Compute) {
         super(scope, 'gateway');
 
-        const rest = new api.RestApi(this, 'rest');
+        const rest = new api.RestApi(this, 'rest', {
+            defaultCorsPreflightOptions: {
+                allowOrigins: api.Cors.ALL_ORIGINS,
+                allowMethods: api.Cors.ALL_METHODS // this is also the default
+            },
+        });
         const base = new api.Resource(this, 'api', {
             parent: rest.root,
             pathPart: 'api',
-        });
-        base.addCorsPreflight({
-            allowOrigins: api.Cors.ALL_ORIGINS,
         });
         const operate = new api.Resource(this, 'operate', {
             parent: base,
             pathPart: 'operate',
         });
-        const postOperate = operate.addMethod('POST', new api.MockIntegration({
-            passthroughBehavior: api.PassthroughBehavior.NEVER,
-            requestTemplates: {
-                "application/json": "{\"statusCode\": 200}"
-            },
-            integrationResponses: [
-                {
-                    selectionPattern: "2\\d{2}",
-                    statusCode: "210",
-                    responseTemplates: {
-                        "application/json": ""
-                    },
-
-                }
-            ],
-        }), {
-            methodResponses: [{ statusCode: "210" }],
-        });
+        operate.addMethod('POST', new api.LambdaIntegration(compute.operateFunction));
 
         const status = new api.Resource(this, 'status', {
             parent: base,
             pathPart: 'status',
         });
-        status.addMethod('GET', new api.MockIntegration({
-            passthroughBehavior: api.PassthroughBehavior.NEVER,
-            requestTemplates: {
-                "application/json": "{\"statusCode\": 200}"
-            },
-            integrationResponses: [
-                {
-                    statusCode: "200",
-                    responseTemplates: {
-                        "application/json": "\"Up\""
-                    },
-
-                }
-            ],
-
-        }), {
-            methodResponses: [{ statusCode: "200" }],
-        });
+        status.addMethod('GET', new api.LambdaIntegration(compute.statusFunction));
 
     }
 }
