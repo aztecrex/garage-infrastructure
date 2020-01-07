f6735ae729d3115bdec82c2638ef0df213315e0f
Greg Wiley <gwiley@cj.com>
2020-01-06 16:05:03 -0800

    functions for up/down

--- lib/api.ts
+++ lib/api.ts
@@ -62,24 +62,43 @@ class NodeFunction extends lambda.Function {
             code: lambda.Code.fromInline(code || DUMB_NODE_CODE),
             handler: 'index.handler',
         });
+        // wide open permissions on IoT core. in production, you'd want
+        // to ratchet this down to bare minimum
         this.addToRolePolicy(new iam.PolicyStatement({
             actions: ['iot:*'],
             resources: ['*'],
         }));
+
+        new cdk.CfnOutput(scope, 'LambdaFun-'+id+-'Name', {
+            value: this.functionName
+        });
     }
 }
 
 class Compute extends cdk.Stack {
-    public readonly statusFunction;
-    public readonly operateFunction;
+    public readonly statusFunction: lambda.Function;
+    public readonly operateFunction: lambda.Function;
+    public readonly upFunction: lambda.Function;
+    public readonly downFunction: lambda.Function;
     constructor(scope: cdk.Construct) {
         super(scope, 'compute');
         this.statusFunction = new NodeFunction(this, 'status', CANNED_STATUS_CODE);
         this.operateFunction = new NodeFunction(this, 'operate', CANNED_OPERATE_CODE);
+        this.upFunction = new NodeFunction(this, 'up', CANNED_OPERATE_CODE);
+        this.downFunction = new NodeFunction(this, 'down', CANNED_OPERATE_CODE);
     }
 }
 
+interface MethodSpec {
+    readonly method: string;
+    readonly function: lambda.Function;
+}
+
+
 class Gateway extends cdk.Stack {
+
+    private readonly base: api.Resource;
+
     constructor(scope: cdk.Construct, compute: Compute) {
         super(scope, 'gateway');
 
@@ -89,21 +108,47 @@ class Gateway extends cdk.Stack {
                 allowMethods: api.Cors.ALL_METHODS // this is also the default
             },
         });
-        const base = new api.Resource(this, 'api', {
+        this.base = new api.Resource(this, 'api', {
             parent: rest.root,
             pathPart: 'api',
         });
-        const operate = new api.Resource(this, 'operate', {
-            parent: base,
-            pathPart: 'operate',
-        });
-        operate.addMethod('POST', new api.LambdaIntegration(compute.operateFunction));
 
-        const status = new api.Resource(this, 'status', {
-            parent: base,
-            pathPart: 'status',
-        });
-        status.addMethod('GET', new api.LambdaIntegration(compute.statusFunction));
+        this.addResource('operate', { method: 'POST', function: compute.operateFunction });
+        // const operate = new api.Resource(this, 'operate', {
+        //     parent: this.base,
+        //     pathPart: 'operate',
+        // });
+        // operate.addMethod('POST', new api.LambdaIntegration(compute.operateFunction));
 
+        this.addResource('up', { method: 'POST', function: compute.upFunction });
+        // const up = new api.Resource(this, 'up', {
+        //     parent: this.base,
+        //     pathPart: 'up',
+        // });
+        // up.addMethod('POST', new api.LambdaIntegration(compute.upFunction));
+
+        const down = new api.Resource(this, 'down', {
+            parent: this.base,
+            pathPart: 'down',
+        });
+        down.addMethod('POST', new api.LambdaIntegration(compute.downFunction));
+
+        this.addResource('status', {method: 'GET', function: compute.statusFunction });
+        // const status = new api.Resource(this, 'status', {
+        //     parent: this.base,
+        //     pathPart: 'status',
+        // });
+        // status.addMethod('GET', new api.LambdaIntegration(compute.statusFunction));
     }
+
+    private addResource(path: string, ...methods: MethodSpec[]) {
+        const res = new api.Resource(this, path, {
+            parent: this.base,
+            pathPart: path,
+        });
+        methods.forEach(s => {
+            res.addMethod(s.method, new api.LambdaIntegration(s.function));
+        });
+    }
+
 }
